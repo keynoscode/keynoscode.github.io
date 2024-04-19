b2524379e8aad2be1bb869b7ce0fcc682c08e5fa
Rimuru <456123code@gmail.com>
2024-04-19 19:42:38 +0100

    An update before my exam1.0

---
+++ Javascript/page.js
@@ -0,0 +1,27 @@
+document.getElementById("list").innerHTML = "網站仍在完善中 請等待31415年 謝謝 (所以這個網站31415年後會否嵌在AI大腦中)";
+
+
+
+/*
+const element=document.querySelector('.text');
+const text = window.getComputedStyle();
+const tw= text.getPropertyValue('--textw')
+*/
+
+const pagename='利姆露的動畫列表';
+document.getElementsByClassName("text")[0].innerHTML=pagename;
+
+const pnl=pagename.length;
+console.log(pnl);
+
+/*
+const textx = document.getElementsByClassName("text");
+const textw = textx[0].offsetWidth + 'px';
+console.log(textw)
+const text = document.querySelector('.text');
+text.style.setProperty('--textw', 'textw');
+
+const text = document.querySelector('.text');
+text.style.setProperty('--pnl', 'pnl');
+*/
+
