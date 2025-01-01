e75b44dfa9efed5ef111d6076b12ddf00d2ba97d
Rimuru <456123code@gmail.com>
2025-01-01 23:09:32 +0000

    window.location.replace(targetUrl); // Use replace instead of href

--- js/redirect.js
+++ js/redirect.js
@@ -1,20 +1,16 @@
 
 const redirect = (link) => {
- window.onload = execute(link)
-}
-
-const execute = (link) => {
+    let targetUrl;
     switch(link) {
         case 'homepage':
-            link = '/web/利姆露的網路領域-首頁.html';
+            targetUrl = '/web/利姆露的網路領域-首頁.html';
             break;
         case 'nav':
-            link = '/nav.html';
+            targetUrl = '/nav.html';
             break;
         default:
             console.error('Invalid link specified');
             return;
     }
-    window.location.href = link;
+    window.location.replace(targetUrl); // Use replace instead of href
 }
-
